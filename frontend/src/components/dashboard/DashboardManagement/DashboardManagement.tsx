008ed84aa452b59cc00a0e98e56e08b5ea4a32a7
murasame112 <tomaszwiesek00@gmail.com>
2026-05-12 01:15:25 +0200

    add item button

--- frontend/src/components/dashboard/DashboardManagement/DashboardManagement.tsx
+++ frontend/src/components/dashboard/DashboardManagement/DashboardManagement.tsx
@@ -1,13 +1,14 @@
 import styles from './DashboardManagement.module.css';
 import { useTranslation } from '../../../shared/i18n/useTranslation';
-import { FunnelIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { FunnelIcon, MagnifyingGlassIcon, PlusIcon } from '@heroicons/react/24/outline';
 
 type DashboardManagementProps = {
 	className: string;
+	mode: string;
 }
 
 
-export default function DashboardManagement({className}: DashboardManagementProps) {
+export default function DashboardManagement({className, mode}: DashboardManagementProps) {
 	const { t } = useTranslation();
 	
 	return(
@@ -20,6 +21,7 @@ export default function DashboardManagement({className}: DashboardManagementProp
 				/>
 			</div>
 			<button className={styles.filterButton}><FunnelIcon className={styles.filterIcon}/>{t('addFilter')}</button>
+			<button className={styles.addButton}><PlusIcon className={styles.plusIcon}/>{t('add')} {mode}</button>
 		</div>
 	);
 }
