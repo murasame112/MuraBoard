91fe022c7e03e159bdf6ecc0ad0f72943952dc90
murasame112 <tomaszwiesek00@gmail.com>
2026-05-13 23:43:08 +0200

    company popup

--- frontend/src/components/dashboard/JobOffersDetails/JobOffersDetails.tsx
+++ frontend/src/components/dashboard/JobOffersDetails/JobOffersDetails.tsx
@@ -1,6 +1,7 @@
 import styles from './JobOffersDetails.module.css';
 import type { JobOffer } from '../../../shared/models/models';
 import { useTranslation } from '../../../shared/i18n/useTranslation';
+import { useState } from 'react';
 
 type JobOffersDetailsProps = {
 	jobOffers: JobOffer[];
@@ -8,6 +9,16 @@ type JobOffersDetailsProps = {
 
 export default function JobOffersDetails({jobOffers}: JobOffersDetailsProps){
 	const { t } = useTranslation();
+	const [selectedCompany, setSelectedCompany] = useState<number | null>(null);
+
+	function selectCompany(id: number){
+		setSelectedCompany((prev) => {
+			console.log(prev);
+			if (!prev) return id;
+			return null;
+		})
+	}
+
 	return(
 		<div className={styles.jobOffersList}>
 
@@ -25,12 +36,28 @@ export default function JobOffersDetails({jobOffers}: JobOffersDetailsProps){
 			jobOffers.map((element) => (
 				<div key={element.id} className={styles.jobOfferItem}>
 					<div className={styles.select}><input type='checkbox' name='selectItem'/></div>
-					<p className={styles.companyDetails}>{element.company.name}</p>
+
+					<div className={styles.companyName} onClick={() => {selectCompany(element.id)}}>
+					{element.id === selectedCompany && (
+						<div className={styles.companyDetails}>
+							<p className={styles.companyDetailsName}>{element.company.name}</p>
+							<p>{element.company.location}</p>
+							{element.company.website ? (
+								<a href={element.company.website} className={styles.companyDetailsWebsite}>{element.company.website}</a>
+							) : (
+								<p className={styles.companyDetailsNoWebsite}>{t('noWebsite')}</p>
+							)}
+							<p className={styles.companyDetailsClose}>{t('companyDetailsClose')}</p>
+						</div>
+					)}
+					<span className={styles.companyNameText}>{element.company.name}</span>
+				</div>
+
 					<p className={`${styles.offerStatus} ${element.application ? styles.applied : styles.notApplied }`}>{element.application ? t('applied'): t('notApplied') }</p>
 					<p>{element.title}</p>
 					<p>{element.salaryMin} - {element.salaryMax}</p>
 					<p>{element.currency}</p>
-					<p>{/*element.createdAt.toLocaleDateString('pl-PL')*/}</p>
+					<p>{new Date(element.createdAt).toLocaleDateString('pl-PL')}</p>
 				</div>
 			))}
 		</div>
