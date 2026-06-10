e9f1442cfbdc34869a41a90e8f1d6c3a63ba1a00
murasame112 <tomaszwiesek00@gmail.com>
2026-06-10 16:06:03 +0200

    moved button:disabled to index.css + pager display

--- frontend/src/features/dashboard/DashboardPager/DashboardPager.tsx
+++ frontend/src/features/dashboard/DashboardPager/DashboardPager.tsx
@@ -1,5 +1,7 @@
 import styles from './DashboardPager.module.css';
-import { useState, useEffect } from 'react';
+import { ChevronDoubleLeftIcon, ChevronDoubleRightIcon, ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
+import { useTranslation } from '../../../shared/i18n/useTranslation';
+import { useState } from 'react';
 
 type DashboardPagerType = {
 	recordCount: number;
@@ -9,9 +11,11 @@ type DashboardPagerType = {
 }
 
 export default function DashboardPager({recordCount, currentPage, pageSize, onPageChange}: DashboardPagerType) {
+	const { t } = useTranslation();
 	const lastPage = Math.ceil(recordCount/pageSize);
 	let start: number;
 	const slots: number[] = [];
+	const [goToPageValue, setGoToPageValue] = useState<number>();
 	
 	if (lastPage <= 5){ 
 		start = 1;
@@ -29,11 +33,36 @@ export default function DashboardPager({recordCount, currentPage, pageSize, onPa
 		slots.push(start + i);
 	}
 
+	function handleChange(e: React.ChangeEvent<HTMLInputElement>){
+
+	}
+
 	return (
-		<div className={styles.pager}>
-			{slots.map((element) => (
-				<div key={element} className={element === currentPage ? styles.pageActive : (element > lastPage ? styles.pageDisabled : styles.page)}>{element}</div>
-			))}
+		<div className={styles.pagerWrapper}>
+			<div className={styles.pager}>
+				<div className={styles.pagesBox}>
+					<ChevronDoubleLeftIcon className={`${styles.arrowButton} ${currentPage === 1 ? styles.arrowDisabled : styles.arrowActive}`}/>
+					<ChevronLeftIcon className={`${styles.arrowButton} ${currentPage === 1 ? styles.arrowDisabled : styles.arrowActive}`}/>
+
+					{slots.map((element) => (
+						<div key={element} className={`${element === currentPage ? styles.pageActive : (element > lastPage ? styles.pageDisabled : '')} ${styles.page}`}>
+							{element}
+						</div>
+					))}
+
+					
+					<ChevronRightIcon className={`${styles.arrowButton} ${currentPage === lastPage ? styles.arrowDisabled : styles.arrowActive}`}/>
+					<ChevronDoubleRightIcon className={`${styles.arrowButton} ${currentPage === lastPage ? styles.arrowDisabled : styles.arrowActive}`}/>
+
+
+				</div>
+				<div className={styles.goToPageBox}>
+					<label htmlFor='selectPage'>{t('goToPage')}:</label>
+					<input id='selectPage' type='text' onChange={handleChange} />
+					<button type='button' onClick={() => onPageChange(goToPageValue!)} disabled={goToPageValue ? false : true}>{t('go')}</button>
+				</div>
+
+			</div>
 		</div>
 	);
 }
