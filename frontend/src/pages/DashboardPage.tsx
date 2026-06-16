68ee6d11a069f0f34f48ca5a4640ce17ccbbf801
murasame112 <tomaszwiesek00@gmail.com>
2026-06-16 16:28:55 +0200

    set filters now allows to apply new value on filter thats already added

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -96,12 +96,26 @@ export default function DashboardPage({mode}: DashboardPageProps){
 
 	function setFilter(filter: Filter) {
 		setRefreshToken((prev) => prev + 1);
+
 		setQueryState((prev) => {
-			const alreadyExists = prev.filters.some((element) => element.filterName === filter.filterName);
+			const existingFilter = prev.filters.find(
+				(element) => element.filterName === filter.filterName
+			);
 
-			if (alreadyExists) return {...prev};
+			if (existingFilter?.value === filter.value) {
+				return prev;
+			}
 
-			return {...prev, currentPage: 1, filters: [...prev.filters, filter]};
+			return {
+				...prev,
+				currentPage: 1,
+				filters: [
+					...prev.filters.filter(
+						(element) => element.filterName !== filter.filterName
+					),
+					filter,
+				],
+			};
 		});
 	}
 
