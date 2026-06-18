956ec405a4e9a8f450ed1a9f6a2e5e3e60f2e186
murasame112 <tomaszwiesek00@gmail.com>
2026-06-18 11:54:36 +0200

    buildQueryParams

---
+++ frontend/src/shared/lib/buildQueryParams.ts
@@ -0,0 +1,19 @@
+import type { QueryState } from "../../features/dashboard/models/queryState";
+
+export function buildQueryParams(userId: number, queryState: QueryState) {
+	const params = new URLSearchParams();
+
+	params.append('userId', String(userId));
+
+	if (queryState.searchPhrase) {
+		params.append('searchPhrase', queryState.searchPhrase);
+	}
+
+	queryState.filters.forEach((filter) => {
+		if (filter.value !== undefined) {
+			params.append(filter.filterName, String(filter.value));
+		}
+	});
+
+	return params.toString();
+}
